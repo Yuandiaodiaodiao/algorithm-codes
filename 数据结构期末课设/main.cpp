3cfba7395a60a8a35733fb1deab4e2479cdd030e
Yuan-ZH <qq295087430@outlook.com>
2019-05-22 15:13:05 +0800

    Update main.cpp

--- 数据结构期末课设/main.cpp
+++ 数据结构期末课设/main.cpp
@@ -8,6 +8,7 @@ public:
 	HashList(int maxs = 100007) {
 		maxsize = maxs;
 		array = new string[maxs];
+		array2 = new string[maxs];
 		bin = new ull[maxs];
 		bin[0] = 1;
 		for (int a = 1; a < maxs; ++a) {
@@ -19,8 +20,14 @@ public:
 		delete[]array;
 	}
 	void add(string s) {
-		if (!array[hash(s) % maxsize].empty())cerr << "hash crashed" << endl;
-		array[hash(s) % maxsize] = s;
+		if (!array[hash(s) % maxsize].empty()) {
+			array2[hash(array[hash(s) % maxsize]) % (maxsize - 10)] = array[hash(s) % maxsize];
+			array2[hash(s) % (maxsize - 10)] = s;
+			array[hash(s) % maxsize].clear();
+		}
+		else {
+			array[hash(s) % maxsize]=s;
+		}
 		cout << "¹þÏ£Öµ" << hash(s) << endl;
 	}
 	ull hash(string& s) {
@@ -31,7 +38,10 @@ public:
 		return hashint;
 	}
 	string& find(ull hashint) {
-		return array[hashint % maxsize];
+		if (array[hashint % maxsize].empty()) {
+			return array2[hashint % (maxsize - 10)];
+		}
+		else return array[hashint % maxsize];
 	}
 	ull getsub(int l, int r, ull * hasha) {
 		if (r < l)return 0;
@@ -41,10 +51,21 @@ public:
 	const ull hashP = 131;
 private:
 	string* array;
+	string* array2;
 
 
 };
 int hashDone[100008];
+
+void solvedone(HashList& hashstr, int ac, ull hashi) {
+	if (hashDone[hashi % hashstr.maxsize] != ac) {
+		string& ans = hashstr.find(hashi);
+		if (!ans.empty()) {
+			cout << ans << endl;
+		}
+		hashDone[hashi % hashstr.maxsize] = ac;
+	}
+}
 int main() {
 	int n;
 	cin >> n;
@@ -74,38 +95,19 @@ int main() {
 				if(a<len)
 				{
 					ull hashi = hasharray[len - 1] + (chr - s[a]) * hashstr.bin[len - 1 - a];
-					if (hashDone[hashi % hashstr.maxsize] != ac) {
-						string& ans = hashstr.find(hashi);
-						if (!ans.empty()) {
-							cout << ans << endl;
-						}
-						hashDone[hashi % hashstr.maxsize] = ac;
-					}
-
+					solvedone(hashstr, ac, hashi);
 				}
 				//insert
 				{
 					ull hashi = ((a - 1 >= 0) ? hasharray[a - 1] * hashstr.bin[len  - (a - 1) ] : 0) + (chr - 'a'+1) * hashstr.bin[len - a] + hashstr.getsub(a, len - 1, hasharray);
-					if (hashDone[hashi % hashstr.maxsize] != ac) {
-						string& ans = hashstr.find(hashi);
-						if (!ans.empty()) {
-							cout << ans << endl;
-						}
-						hashDone[hashi % hashstr.maxsize] = ac;
-					}
+					solvedone(hashstr, ac, hashi);
 				}
 			}
 			
 			//delete
 			{
 				ull hashi = ((a - 1 >= 0) ? hasharray[a - 1] * hashstr.bin[len - 1 - (a - 1) - 1] : 0) + hashstr.getsub(a + 1, len - 1, hasharray);
-				if (hashDone[hashi % hashstr.maxsize] != ac) {
-					string& ans = hashstr.find(hashi);
-					if (!ans.empty()) {
-						cout << ans << endl;
-					}
-					hashDone[hashi % hashstr.maxsize] = ac;
-				}
+				solvedone(hashstr, ac, hashi);
 			}
 		}
 		delete[]hasharray;
